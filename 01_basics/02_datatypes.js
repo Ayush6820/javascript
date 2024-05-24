4070ac974e07cea43fec7a3d0f835e69122cb5af
Ayush Kumar <98230831+Ayush6820@users.noreply.github.com>
2024-05-24 20:33:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as a newer version  
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+// code readability should be high
+console.log("Ayush")
+
+//types of Datatypes
+/*(Primitive DataTypes)
+number => 2 ^53(range)
+bigint(use this for big numbers specially for trading etc..)
+string=> " "(mostly used) or' ' (we can use both single bracket as well as double bracket)
+boolean => ture/false
+null => standalone value (reprentation of mpty value)
+undefined => yet the value is not defined
+symbol => to define uniqueness 
+
+*/
+
+//OBJECT
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
